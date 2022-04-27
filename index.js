63f68ba0226ada1e6f0e0d9ac8fb9cbe278983ab
satish.chandrashekar <satish.chandrashekar@gmail.com>
2022-04-28 09:42:12 +1000

    Add Input Parameters

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const fs = require('fs');
+
 
 (
     async() => {
         try {
             core.notice("skc testing action");
+            const gname = core.getInput('greeting-name');
+            console.log(`Hello + ${gname}`);
+            const time = (new Date().toTimeString);
+            core.setOutput("time", time);
+            const payload = JSON.stringify(github.context.payload, undefined, 2);
+            console.log(`The Event Payload is : ${payload}`);
         } catch (error) {
             core.setFailed(error.message);
         }
