0da6f3021a7658d9d13fce176aacbf338d7ab8f3
satish.chandrashekar <satish.chandrashekar@gmail.com>
2022-04-28 17:49:43 +1000

    error correction

--- index.js
+++ index.js
@@ -11,12 +11,26 @@ const path = require('path');
             const gname = core.getInput('greeting-name');
             console.log(`Hello ${gname}`);
             const objects = "./force-app/main/default/objects";
+            const flows = "./force-app/main/default/flows";
+            console.log("Custom Object List");
             fs.readdir(objects, (err, files) => {
+                console.log(files.length);
                 if (err) {
                     return console.log('Unable to Scan Directory or Not Directory Fouund' + err);
                 }
                 files.forEach(function(file) {
                     console.log(file);
+                })
+            })
+
+            console.log("Extracting Flows");
+            fs.readdir(flows, (err, lfs) => {
+                console.log(lfs.length);
+                if (err) {
+                    return console.log('Unable to Scan Directory or Not Directory Fouund' + err);
+                }
+                lfs.forEach(function(lf) {
+                    console.log(lf);
                 })
             })
         } catch (error) {
