5997671ef97055065965bcdbb9f07c0b74f94343
satish.chandrashekar <satish.chandrashekar@gmail.com>
2022-05-02 11:55:55 +1000

    Corrected Path

--- index.js
+++ index.js
@@ -13,8 +13,7 @@ async function checkConfigFile(filePath) {
             // Call other functions to read / Execute other Rules.
             fs.readFile(filePath, 'utf-8', (err, data) => {
                 if (err) { core.setFailed(`Error Reading JSON file`) }
-                ruleset = JSON.parse(data);
-                core.info(ruleset);
+                core.info(data);
             })
             return true;
         })
