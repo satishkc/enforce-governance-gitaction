aa87370f031c2409d57fb89740c2065671be64e3
satish.chandrashekar <satish.chandrashekar@gmail.com>
2022-05-04 11:41:30 +1000

    add XML Parser

--- Rules/automationrules.js
+++ Rules/automationrules.js
@@ -64,7 +64,7 @@ function getflowmeta(value) {
             console.log(file);
             var tstr = file;
             const pb = new DOMParser().parseFromString(tstr);
-            const ispb = pb.getElementsById("start").textContent;
+            const ispb = pb.getElementsByName("start").textContent;
             console.log(ispb);
             /*if (ispb.length > 0) {
                 console.log('This is a lightning flow');
